54bf1d9562e25760a4acd63bd211fe8056003e41
M-in <dependence.letgo@gmail.com>
2018-10-09 14:45:25 +0800

    增加获取单个theme的接口

--- app/api/v1/theme.py
+++ app/api/v1/theme.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import jsonify
 
+from app.libs.exceptions import NotFound
 from app.libs.redprint import Redprint
 from app.models.theme import Theme
 from app.serializer.theme import ThemeItemSchema
@@ -18,3 +19,15 @@ def read_all_theme():
         return jsonify(result)
     else:
         return jsonify({})
+
+
+@api.route('/<int:theme_id>', methods=['GET'])
+def read_one_theme(theme_id):
+    theme = Theme.query.filter_by(id=theme_id).first()
+
+    if theme is not None:
+        theme_schema = ThemeItemSchema()
+        result = theme_schema.dump(theme).data
+        return jsonify(result)
+    else:
+        raise NotFound(f'Theme not found for Id {theme_id}')
--- app/serializer/theme.py
+++ app/serializer/theme.py
@@ -6,6 +6,7 @@ from . import ma
 
 class ThemeItemSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'description', 'topic_img')
+        fields = ('id', 'name', 'description', 'topic_img', 'head_img')
 
     topic_img = ma.Nested(ImageSchema)
+    head_img = ma.Nested(ImageSchema)
