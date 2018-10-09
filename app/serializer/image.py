6c67769c797fd801572f08e5e2f21e8d33b71150
M-in <dependence.letgo@gmail.com>
2018-10-09 09:07:28 +0800

    配置图片资源url

--- app/serializer/image.py
+++ app/serializer/image.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
+from flask import request, url_for
+from marshmallow import post_dump
 
 from . import ma
 
-class ImageSchema(ma.Schema):
 
+class ImageSchema(ma.Schema):
     class Meta:
         fields = ('url',)
+
+    @post_dump
+    def get_url(self, item):
+        url = item.get('url')
+        base_api = request.host_url.rstrip('/')
+        item['url'] = base_api + url_for('static', filename='images' + url)
+        return item
